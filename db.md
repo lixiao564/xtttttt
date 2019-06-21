4a0c344c0dfb33a94f801819756c9c0cd5222640
李逍 <lixiao@lixiaodeMacBook-Pro.local>
2019-06-21 15:56:59 +0800

    客户管理的增、删、查

--- db.md
+++ db.md
@@ -1,6 +1,6 @@
 ## User
 
-#### 基本字段
+### 基本字段
 
 - `_id` 数据表的唯一表示（数据库自动生成）
 - `name` 姓名
@@ -10,15 +10,17 @@
 - `dpt` 部门
 - `role` 角色
 
-#### 新增
+### 新增
 
 `/user/add`
 
-#### 列表
+手机号不能重复
+
+### 列表
 
 `/user/list`
 
-#### 删除
+### 删除
 
 `/user/delete`
 
@@ -26,4 +28,33 @@
 
 参数值：`Array`类型
 
-可以同时删除多个，多个_id用分号拼接
+## Customer
+
+#### 基本字段
+
+- `_id` 数据表的唯一表示（数据库自动生成）
+- `uid` 所属客户经理的 _id 
+- `name` 客户名称
+- `customerType` 客户类型
+- `industryType` 行业类型
+- `area` 所属区域
+- `validDate` 有效期
+- `basicInfo` 单位基本信息
+
+### 新增
+
+`/customer/add`
+
+客户名称不能重复
+
+### 列表
+
+`/customer/list`
+
+### 删除
+
+`/customer/delete`
+
+参数名：`_id`
+
+参数值：`Array`类型
--- server/router/customer.js
+++ server/router/customer.js
@@ -1,11 +1,21 @@
 const express = require('express'),
+      path = require('path'),
       router = express.Router(),
       utils = require('../utils/dbUtils');
 
 const customerModel = utils.initSchema({
+    uid: String,
     name: String,
-    type: String,
+    customerType: String,
+    industryType: String,
+    area: String,
+    validDate: String,
+    basicInfo: String
 }, 'customer');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/customer/customer.html'));
+});
 
 router.get('/list', (req, res) => {
     utils.find(customerModel, {}).then(data => {
@@ -20,6 +30,42 @@ router.get('/list', (req, res) => {
     }).catch(err => {
         console.log(err);
     });
+});
+
+router.post('/add', (req, res) => {
+    const body = req.body;
+    utils.add(customerModel, body, {
+        name: body.name
+    }).then(data => {
+        res.send({
+            code: 0,
+            data: data,
+            msg: ''
+        });
+    }).catch(err => {
+        res.send({
+            code: -1,
+            data: '',
+            msg: err
+        });
+    });
+});
+
+router.delete('/delete', (req, res) => {
+    const _id = req.body._id;
+    utils.delete(customerModel, _id).then(data => {
+        res.send({
+            code: 0,
+            data: data,
+            msg: ''
+        });
+    }).catch(err => {
+        res.send({
+            code: -1,
+            data: '',
+            msg: err
+        });
+    })
 });
 
 module.exports = router;
